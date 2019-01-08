171fbb0c0372321773e339b7fba1f584428d597c
Manoj <manojkumarupm60@gmail.com>
2019-01-08 06:44:58 -0500

    Commiting the changes for further proceeding

---
+++ TaskManager.Services/Controllers/TaskController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using TaskManager.BL;
+using TaskManager.DAL;
+namespace TaskManager.Services.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class TaskController : ApiController
+    {
+        public TaskCrud TaskDetailsGetter { get; set; }
+        public TaskController()
+        {
+            TaskDetailsGetter = new TaskCrud();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<TaskInformation> Get()
+        {
+            return TaskDetailsGetter.GetAllTasks();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="TaskId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public HttpResponseMessage Get(int TaskId)
+        {
+
+            TaskInformation i = TaskDetailsGetter.GetTask(TaskId);
+            if (i != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, i);
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Task Id : " + TaskId + " Not Found");
+            }
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public HttpResponseMessage Post([FromBody] TaskInformation i)
+        {
+            string Result = null;
+            try
+            {
+                Result = TaskDetailsGetter.AddTask(i);
+                if (Result.Equals("Success"))
+                {
+                    HttpResponseMessage Message = Request.CreateResponse(HttpStatusCode.Created, i);
+                    return Message;
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public HttpResponseMessage Put([FromBody] TaskInformation i)
+        {
+            string Result = null;
+            try
+            {
+                Result = TaskDetailsGetter.UpdateTask(i);
+                if (Result.Equals("Success"))
+                {
+                    HttpResponseMessage Message = Request.CreateResponse(HttpStatusCode.OK, i);
+                    return Message;
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, Result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="TaskId"></param>
+        /// <returns></returns>
+        public HttpResponseMessage Delete(int TaskId)
+        {
+            string Result = null;
+            try
+            {
+                Result = TaskDetailsGetter.RemoveTask(TaskId);
+                if (Result.Equals("Success"))
+                {
+                    HttpResponseMessage Message = Request.CreateResponse(HttpStatusCode.OK, Result);
+                    return Message;
+                }
+                else if (Result.Contains(" Not found"))
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, Result);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, Result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+    }
+}
